053633a97de7eedbc1d00725fd9c817c6f2ac0fb
keagdo <keagdo@users.noreply.github.com>
2025-03-22 00:20:08 -0400

    initial commit to set up project

--- EdenHall/Plugin.cs
+++ EdenHall/Plugin.cs
@@ -4,9 +4,9 @@ using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using EdenHall.Windows;
 
-namespace SamplePlugin;
+namespace EdenHall;
 
 public sealed class Plugin : IDalamudPlugin
 {
@@ -17,11 +17,11 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
+    private const string CommandName = "/ebj";
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("EdenHall");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
@@ -54,8 +54,8 @@ public sealed class Plugin : IDalamudPlugin
 
         // Add a simple message to the log with level set to information
         // Use /xllog to open the log window in-game
-        // Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
-        Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+        // Example Output: 00:57:54.959 | INF | [EdenHall] ===A cool log message from Sample Plugin===
+        Log.Information($"=== Started {PluginInterface.Manifest.Name}===");
     }
 
     public void Dispose()
